efd52f5300648ed17ad3acfd32591af4fc59a2b2
mahnoorfarhat1 <143686849+mahnoorfarhat1@users.noreply.github.com>
2026-02-26 10:06:53 +0500

    Delete Section B directory

--- Section B/2023-EE-24 Bilal Ahmad/src/lab1.sv
+++
@@ -1,8 +0,0 @@
-module lab1
-(
-    input logic A,
-    input logic B,
-    output logic Y
-) ;
-    assign Y = A & B ;
-endmodule
--- Section B/Alina_Hassan_2024-EE-51/DSD_LAB_1/src/lab1_AND_GATE.sv
+++
@@ -1,8 +0,0 @@
-module lab1_AND_GATE
-(
-input  A,
-input B,
-output Y
-);
-assign Y=A&B;
-endmodule
